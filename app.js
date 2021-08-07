a8203820e026633d4aa8fb8209c3c74669412768
Aishwary Shukla <aishwaryshukla@Starker7.local>
2021-08-07 15:06:28 +0530

    Implemented authentication with cookies using passport.js

--- app.js
+++ app.js
@@ -1,79 +1,106 @@
 //jshint esversion:6
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express();
 
-
 //This specifies a static folder
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
 
-const userSchema = new mongoose.Schema ({
-    email: String,
-    password: String
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+userSchema.plugin(passportLocalMongoose);
 
 //our new user model
 const User = new mongoose.model("User", userSchema);
 
-app.get("/", function(req, res){
-    res.render("home");
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get("/login", function(req, res){
-    res.render("login");
+app.get("/login", function (req, res) {
+  res.render("login");
 });
 
-app.get("/register", function(req, res){
-    res.render("register");
+app.get("/register", function (req, res) {
+  res.render("register");
 });
 
-app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()){
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }   
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+app.post("/register", function (req, res) {
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
+
+
+app.post("/login", function (req, res) {
+    const user = new User({
+        username: req.body.username,
         password: req.body.password
     });
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    });
-
-});
-
-app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function(err, foundUser){
+    req.login(user, function(err){
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
-            }
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
         }
     });
+  
 });
 
-
-
-app.listen(3000, function(){
-    console.log("Server started on port 3000.");
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
 });
